3e4c168b074ea6ee1532de0da9cb800b9a3145c6
fmuno003 <fmuno003@ucr.edu>
2016-03-28 12:50:21 -0700

    Added files via upload

---
+++ Lab4/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+
+using namespace std;
+
+class Rational
+{
+    private:
+        int numerator;
+        int denominator;
+    public:
+        Rational(); // done
+        Rational(int); // done
+        Rational(int,int); // done
+        const Rational add(const Rational &) const;
+        const Rational subtract(const Rational &) const;
+        const Rational multiply(const Rational &) const;
+        const Rational divide(const Rational &) const;
+        void display() const;
+};
+int main()
+{
+    Rational n;
+    Rational d;
+    Rational a;
+    Rational b;
+    Rational c;
+    Rational e;
+    a = n.add(d);
+    b = n.subtract(d);
+    c = n.multiply(d);
+    e = n.divide(d);
+    j.Rational(5,3);
+    return 0;
+}
+Rational::Rational()
+{
+    numerator = 0;
+    denominator = 1;
+}
+Rational::Rational(int n, int d)
+{
+    numerator = n;
+    denominator = d;
+}
+Rational::Rational(int n)
+{
+    numerator = n;
+    denominator = 1;
+}
+const Rational Rational::add(const Rational &d) const
+{
+    Rational j;
+    j.numerator = ((numerator * d.denominator) + (denominator * d.numerator));
+    j.denominator = denominator * d.denominator;
+    return j;
+}
+const Rational Rational::subtract(const Rational &d) const
+{
+    Rational j;
+    j.numerator = ((numerator * d.denominator) - (denominator * d.numerator));
+    j.denominator = denominator * d.denominator;
+    return j;
+}
+const Rational Rational::multiply(const Rational &d) const
+{
+    Rational J;
+    J.numerator = numerator * d.numerator;
+    J.denominator = denominator * d.denominator;
+    return J;
+}
+const Rational Rational::divide(const Rational &d) const
+{
+    Rational J;
+    J.numerator = numerator * d.denominator;
+    J.denominator = denominator * d.numerator;
+    return J;
+}
+void Rational::display() const
+{
+    cout << numerator << '/' << denominator;
+    return;
+}
