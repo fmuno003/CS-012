ee5ab93321e8fe9ed2e87251f017dd421d3e7842
fmuno003 <fmuno003@ucr.edu>
2016-03-28 12:40:55 -0700

    Added files via upload

---
+++ ex1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int genRandInt(int, int);
+
+int main()
+{
+  srand(time(0));
+  
+    int number_to_guess = genRandInt(1,100);
+    
+    cout << "Guess a number between 1 and 100." << endl;
+
+    int guess;
+    cin >> guess;
+
+    while ( number_to_guess != guess )
+    {
+      if ( guess >= number_to_guess )
+      {
+        cout << "Try lower." << endl << "Guess again: ";
+      }
+      else if ( guess <= number_to_guess )
+      {
+        cout << "Try Higher." << endl << "Guess again: ";
+      }
+      cin >> guess;
+    }
+    cout << "You guessed right!!! Congratulations!" << endl;
+    
+    return 0;
+}
+
+int genRandInt(int low, int high)
+{
+  return low + rand() % (high - low + 1);
+}
