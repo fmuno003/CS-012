71eaf7c21b4b23fd7eb2a864156ad6936d8e4319
fmuno003 <fmuno003@ucr.edu>
2016-03-28 12:51:39 -0700

    Added files via upload

---
+++ Lab3/ex1.cpp
@@ -0,0 +1,64 @@
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const int ROWS = 2;
+const int COLS = 3;
+
+void matrixAdd(const int a[][COLS], const int b[][COLS], int sum[][COLS]);
+void displayMatrix(const int sumMatrix[][COLS]);
+void inputMatrix(int a[][COLS]);
+
+int main()
+{
+    int matrixA[ROWS][COLS] = {0};
+    int matrixB[ROWS][COLS] = {0};
+    int totalMatrix[ROWS][COLS] = {0};
+    
+    cout << "Enter Matrix A: ";
+    inputMatrix(matrixA);
+    
+    cout << "Enter Matrix B: ";
+    inputMatrix(matrixB);
+    
+    matrixAdd(matrixA, matrixB, totalMatrix);
+    cout << "Result: " << endl;
+    displayMatrix(totalMatrix);
+    
+        
+    return 0;
+}
+
+void matrixAdd(const int a[ROWS][COLS], const int b[ROWS][COLS], int sum[ROWS][COLS])
+{
+    for (int i = 0; i < ROWS; ++i)
+    {
+        for(int j = 0; j < COLS; ++j)
+        {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+}
+void displayMatrix(const int sumMatrix[ROWS][COLS])
+{
+    for (int i = 0; i < ROWS; ++i)
+    {
+        for(int j = 0; j < COLS; ++j)
+        {
+            cout << sumMatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void inputMatrix(int matrixInput[ROWS][COLS])
+{
+    for(int i = 0; i < ROWS; ++i)
+    {
+        for (int j = 0; j < COLS; ++j)
+        {
+            cin >> matrixInput[i][j];
+        }
+    }
+}
