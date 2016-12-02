65038bb15c243d4613370e6d376b6b726aacee5d
Francisco <fmuno003@ucr.edu>
2016-12-01 17:58:45 -0800

    Optimized

--- Lab1/ex1.cpp
+++ Lab1/ex1.cpp
@@ -6,35 +6,27 @@ using namespace std;
 
 int genRandInt(int, int);
 
+int generateRandom(int, low, int high)
+{
+		return low + rand() % (high - low + 1);
+}
 int main()
 {
-  srand(time(0));
+	srand(time(0));
   
-    int number_to_guess = genRandInt(1,100);
-    
-    cout << "Guess a number between 1 and 100." << endl;
+	int number_to_guess = genRandInt(1,100);    
+	cout << "Guess a number between 1 and 100." << endl;
+	int guess;
+	cin >> guess;
 
-    int guess;
-    cin >> guess;
-
-    while ( number_to_guess != guess )
-    {
-      if ( guess >= number_to_guess )
-      {
-        cout << "Try lower." << endl << "Guess again: ";
-      }
-      else if ( guess <= number_to_guess )
-      {
-        cout << "Try Higher." << endl << "Guess again: ";
-      }
-      cin >> guess;
-    }
-    cout << "You guessed right!!! Congratulations!" << endl;
-    
+	while (number_to_guess != guess)
+	{
+		if (guess >= number_to_guess)
+			cout << "Try lower." << endl << "Guess again: ";
+		else if (guess <= number_to_guess)
+			cout << "Try Higher." << endl << "Guess again: ";
+		cin >> guess;
+	}
+	cout << "You guessed right!!! Congratulations!" << endl;
     return 0;
-}
-
-int genRandInt(int low, int high)
-{
-  return low + rand() % (high - low + 1);
 }
--- Lab1/ex3.cpp
+++ Lab1/ex3.cpp
@@ -4,72 +4,50 @@
 
 using namespace std;
 
-int findValue (const vector<int>& V, int G);
-void removeValue (vector<int>& V, int H);
-void Display (const vector<int>& V);
-
-int main()
-{
-    int userNum = 1;
-    int userNum2;
-    vector<int> userVector;
-    cout << "Please enter in integers (0 to stop):" << endl;
-    while (userNum != 0)
-    {
-        cin >> userNum;
-        if (userNum != 0)
-        {
-            userVector.push_back(userNum);   
-        }
-    }
-    cout << "What number would you like to find and remove?: ";
-    cin >> userNum2;
-    
-    int valuePosition = findValue(userVector, userNum2);
-    if (valuePosition >= 0)
-    {
-        cout << "Found: " << valuePosition << endl;
-        removeValue(userVector, valuePosition);
-    }
-    else
-    {
-        cout << "Found: " << valuePosition << endl;
-    }
-        cout << "Result: "; 
-        Display(userVector);
-        cout << endl;
-        
-    return 0;
-}
-
 void Display(const vector<int>& V)
 {
-    for (int i = 0; i < V.size(); ++i)
-    {
-        cout << V.at(i) << " ";
-    }
-    cout << endl;
-    return;
+	for(int i = 0; i < V.size(); ++i)
+		cout << v.at() << " ";
+	cout << endl;
 }
-
 int findValue(const vector<int>& V, int G)
 {
-    for (int i = 0; i < V.size(); ++i)
-    {
-        if(V.at(i) == G)
-        {
-            return i;
-        }
-    }
-    return -1;
+	for(int i = 0; i < V.size(); ++i)
+		if(V.at(i) == G)
+			return i;
+	return -1;
 }
-
-void removeValue(vector<int>& V, int H)
+void removeValue(vecotr<int>& V, int H)
 {
-    for(int i = H; i + 1 < V.size(); ++i)
-    {
-        V.at(i) = V.at(i + 1);
-    }
-    V.pop_back();
-    return;
+	for(int i = H; i + 1 < V.size(); ++i)
+		V.at(i) = V.at(i + 1);
+	V.pop_back();
+}
+int main()
+{
+	int userNum = 1;
+	int userNum2;
+	vector<int> userVector;
+	cout << "Please enter in integers (0 to stop):" << endl;
+	while (userNum != 0)
+	{
+		cin >> userNum;
+		if (userNum != 0)
+			userVector.push_back(userNum);
+	}
+	cout << "What number would you like to find and remove?: ";
+	cin >> userNum2;
+    
+	int valuePosition = findValue(userVector, userNum2);
+	if (valuePosition >= 0)
+	{
+		cout << "Found: " << valuePosition << endl;
+		removeValue(userVector, valuePosition);
+	}
+	else
+		cout << "Found: " << valuePosition << endl;
+	cout << "Result: "; 
+	Display(userVector);
+	cout << endl;    
+    return 0;
 }
