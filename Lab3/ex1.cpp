e2f8d3a11ea27dec33aea36956ba97c64c160f7b
Francisco <fmuno003@ucr.edu>
2016-12-01 18:17:25 -0800

    Optimized

--- Lab3/ex1.cpp
+++ Lab3/ex1.cpp
@@ -6,59 +6,40 @@ using namespace std;
 const int ROWS = 2;
 const int COLS = 3;
 
-void matrixAdd(const int a[][COLS], const int b[][COLS], int sum[][COLS]);
-void displayMatrix(const int sumMatrix[][COLS]);
-void inputMatrix(int a[][COLS]);
-
-int main()
-{
-    int matrixA[ROWS][COLS] = {0};
-    int matrixB[ROWS][COLS] = {0};
-    int totalMatrix[ROWS][COLS] = {0};
-    
-    cout << "Enter Matrix A: ";
-    inputMatrix(matrixA);
-    
-    cout << "Enter Matrix B: ";
-    inputMatrix(matrixB);
-    
-    matrixAdd(matrixA, matrixB, totalMatrix);
-    cout << "Result: " << endl;
-    displayMatrix(totalMatrix);
-    
-        
-    return 0;
-}
-
 void matrixAdd(const int a[ROWS][COLS], const int b[ROWS][COLS], int sum[ROWS][COLS])
 {
-    for (int i = 0; i < ROWS; ++i)
-    {
-        for(int j = 0; j < COLS; ++j)
-        {
-            sum[i][j] = a[i][j] + b[i][j];
-        }
-    }
+	for(int i = 0; i < ROWS; ++i)
+		for(int j = 0; j < COLS; ++j)
+			sum[i][j] = a[i][j] + b[i][j];
 }
-void displayMatrix(const int sumMatrix[ROWS][COLS])
+void displayMatrix(const in sumMatrix[ROWS][COLS])
 {
-    for (int i = 0; i < ROWS; ++i)
-    {
-        for(int j = 0; j < COLS; ++j)
-        {
-            cout << sumMatrix[i][j] << " ";
-        }
-        cout << endl;
-    }
+	for(int i = 0; i < ROWS; ++i)
+		for(int j = 0; j < COLS; ++j)
+			cout << sumMatrix[i][j] << " ";
+	cout << endl;
 }
-
-void inputMatrix(int matrixInput[ROWS][COLS])
+void inputMatrix(int matrixInptu[ROWS][COLS])
 {
-    for(int i = 0; i < ROWS; ++i)
-    {
-        for (int j = 0; j < COLS; ++j)
-        {
-            cin >> matrixInput[i][j];
-        }
-    }
+	for(int i = 0; i < ROWS; ++i)
+		for(int j = 0; j < COLS; ++j)
+			cin >> matrixInput[i][j];
+}
+int main()
+{
+	int matrixA[ROWS][COLS] = {0};
+	int matrixB[ROWS][COLS] = {0};
+	int totalMatrix[ROWS][COLS] = {0};
+    
+	cout << "Enter Matrix A: ";
+	inputMatrix(matrixA);
+    
+	cout << "Enter Matrix B: ";
+	inputMatrix(matrixB);
+    
+	matrixAdd(matrixA, matrixB, totalMatrix);
+	cout << "Result: " << endl;
+	displayMatrix(totalMatrix);
+    
+	return 0;
 }
--- Lab3/ex2.cpp
+++ Lab3/ex2.cpp
@@ -8,71 +8,48 @@ const int Q = 2;
 const int R = 3;
 const int S = 3;
 
-void matrixMult(const int a[][R], const int b[][S], int product[][S]);
-void displayMatrix(const int productMatrix[][S]);
-void inputMatrixA(int a[][R]); //done
-void inputMatrixB(int b[][S]); //done
-
-
-int main()
-{
-    int matrixA[Q][R] = {0};
-    int matrixB[R][S] = {0};
-    int newMatrix[Q][S] = {0};
-    
-    std::cout << "Enter Matrix A: ";
-    inputMatrixA(matrixA);
-    
-    cout << "Enter Matrix B: ";
-    inputMatrixB(matrixB);
-    
-    matrixMult(matrixA, matrixB, newMatrix);
-    displayMatrix(newMatrix);
-    
-    return 0;
-}
 void inputMatrixA(int a[Q][R])
 {
-    for(int i = 0; i < Q; ++i)
-    {
-        for (int j = 0; j < R; ++j)
-        {
-            cin >> a[i][j];
-        }
-    }
+	for(int i = 0; i < Q; ++i)
+		for(int j = 0; j < R; ++j)
+			cin >> a[i][j];
 }
 void inputMatrixB(int b[R][S])
 {
-    for(int i = 0; i < R; ++i)
-    {
-        for (int j = 0; j < S; ++j)
-        {
-            cin >> b[i][j];
-        }
-    }
+	for(int i = 0; i < R; ++i)
+		for(int j = 0; j < S; ++j)
+			cin >> b[i][j];
 }
-void displayMatrix(const int productMatrix[Q][S])
+void displayMatrix(const int productMAtrix[Q][S])
 {
-    for (int i = 0; i < Q; ++i)
-    {
-        for(int j = 0; j < S; ++j)
-        {
-            cout << productMatrix[i][j] << " ";
-        }
-        cout << endl;
-    }
+	for(int i = 0; i < Q; ++i)
+		for(int j = 0; j <S; ++j)
+			cout << productMatrix[i][j] << " ";
+	cout << endl;
 }
-void matrixMult(const int a[Q][R], const int b[R][S], int product[Q][S])
+void matrixMult(const int a[Q][R], const int b[R][S], int proxuct [Q][S])
 {
-    for (int i = 0; i < Q; ++i)
-    {
-        for (int j = 0; j < R; ++j)
-        {
-            product[i][j] = 0;
-            for (int k = 0; k < S; ++k)
-            {
-                product[i][j] = product[i][j] + (a[i][k] * b[k][j]);
-            }
-        }
-    }
+	for(int i = 0; i < Q; ++i){
+		for(int j = 0; j < R; ++j)
+		{
+			product[i][j] = 0;
+			for(int k = 0; j < S; ++k)
+				product[i][j] += (a[i][k] * b[k][j]);
+		}
+	}
+}
+int main()
+{
+	int matrixA[Q][R] = {0};
+	int matrixB[R][S] = {0};
+	int newMatrix[Q][S] = {0};
+    
+	std::cout << "Enter Matrix A: ";
+	inputMatrixA(matrixA);
+	cout << "Enter Matrix B: ";
+	inputMatrixB(matrixB);
+    
+	matrixMult(matrixA, matrixB, newMatrix);
+	displayMatrix(newMatrix);
+	return 0;
 }
