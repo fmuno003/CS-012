8d8027bd2070b15be07ea715b089b8a2d5188420
Francisco <fmuno003@ucr.edu>
2016-12-01 18:20:29 -0800

    Optimized

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -5,8 +5,7 @@ using namespace std;
 class Rational
 {
     private:
-        int numerator;
-        int denominator;
+        int numerator, denominator;
     public:
         Rational(); // done
         Rational(int); // done
@@ -19,33 +18,28 @@ class Rational
 };
 int main()
 {
-    Rational n;
-    Rational d;
-    Rational a;
-    Rational b;
-    Rational c;
-    Rational e;
-    a = n.add(d);
-    b = n.subtract(d);
-    c = n.multiply(d);
-    e = n.divide(d);
-    j.Rational(5,3);
-    return 0;
+	Rational n, d, a, b, c, e;
+	a = n.add(d);
+	b = n.subtract(d);
+	c = n.multiply(d);
+	e = n.divide(d);
+	j.Rational(5,3);
+	return 0;
 }
 Rational::Rational()
 {
-    numerator = 0;
-    denominator = 1;
+	numerator = 0;
+	denominator = 1;
 }
 Rational::Rational(int n, int d)
 {
-    numerator = n;
-    denominator = d;
+	numerator = n;
+	denominator = d;
 }
 Rational::Rational(int n)
 {
-    numerator = n;
-    denominator = 1;
+	numerator = n;
+	denominator = 1;
 }
 const Rational Rational::add(const Rational &d) const
 {
@@ -63,20 +57,20 @@ const Rational Rational::subtract(const Rational &d) const
 }
 const Rational Rational::multiply(const Rational &d) const
 {
-    Rational J;
-    J.numerator = numerator * d.numerator;
-    J.denominator = denominator * d.denominator;
-    return J;
+	Rational J;
+	J.numerator = numerator * d.numerator;
+	J.denominator = denominator * d.denominator;
+	return J;
 }
 const Rational Rational::divide(const Rational &d) const
 {
-    Rational J;
-    J.numerator = numerator * d.denominator;
-    J.denominator = denominator * d.numerator;
-    return J;
+	Rational J;
+	J.numerator = numerator * d.denominator;
+	J.denominator = denominator * d.numerator;
+	return J;
 }
 void Rational::display() const
 {
-    cout << numerator << '/' << denominator;
-    return;
+	cout << numerator << '/' << denominator;
+	return;
 }
